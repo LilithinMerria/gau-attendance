f7bf038f1eb1c1dc7c1668582366212d2bf6d081
Lilithin Merria <ashlynmerria@gmail.com>
2020-05-27 16:56:59 +0300

    I added templates

--- public/javascripts/change_style.js
+++
@@ -1,8 +0,0 @@
-function seek(){
-    x = document.getElementById("getMe");
-    x.className="seek";
-}
-function hide(){
-    x = document.getElementById("getMe");
-    x.className="hide";
-}
--- public/javascripts/function.js
+++
@@ -1,7 +0,0 @@
-function anEvent(message){
-    document.getElementById('yiha').innerHTML = message + " Event";
-}
-function showDate(){
-    document.getElementById('demo').innerHTML = Date();
-    
-}
